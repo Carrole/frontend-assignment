f6962136f4ceb3263b21c4416b39ecd9b70938a6
Carrole <eundori.dev@gmail.com>
2025-02-11 21:39:30 +0900

    feat: 캐러셀 이미지 클릭 시 외부 링크로 이동하는 기능 추가

--- src/components/common/Carousel.tsx
+++ src/components/common/Carousel.tsx
@@ -3,12 +3,9 @@
 import { useState } from 'react';
 import { useSwipeNavigation } from '@/hooks/useSwipeNavigation';
 import Image from 'next/image';
+import { CarouselDataType } from '@/constants/carouselData';
 
-export const Carousel = ({
-  items,
-}: {
-  items: { title: string; isActive: boolean; period: string; image: string }[];
-}) => {
+export const Carousel = ({ items }: { items: CarouselDataType[] }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const { swipeHandlers } = useSwipeNavigation(
@@ -20,24 +17,28 @@ export const Carousel = ({
   );
 
   return (
-    <div {...swipeHandlers}>
-      <div className="carousel-content">
+    <div>
+      <div {...swipeHandlers} className="carousel-content">
         <span
           className={`font-medium carousel-status ${items[currentIndex].isActive ? 'active' : 'inactive'}`}
         >
           {items[currentIndex].isActive ? '진행 중' : '완료'}
         </span>
-
-        <div className="carousel-image-wrapper">
-          <Image
-            className="carousel-image"
-            src={items[currentIndex].image}
-            alt={items[currentIndex].title}
-            fill
-            objectFit="cover"
-          />
-        </div>
-
+        <a
+          href={items[currentIndex].link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="carousel-image-wrapper">
+            <Image
+              className="carousel-image"
+              src={items[currentIndex].image}
+              alt={items[currentIndex].title}
+              fill
+              objectFit="cover"
+            />
+          </div>
+        </a>
         <div className="carousel-text">
           <div className="font-large ellipsis">{items[currentIndex].title}</div>
           <div className="font-medium carousel-period">
--- src/constants/carouselData.ts
+++ src/constants/carouselData.ts
@@ -1,32 +1,45 @@
-export const CarouselData = [
+export type CarouselDataType = {
+  title: string;
+  isActive: boolean;
+  period: string;
+  image: string;
+  link: string;
+};
+
+export const CarouselData: CarouselDataType[] = [
   {
     title: '[M COUNTDOWN] 10월 2주차 엠카 사전 투표 진행 중',
     isActive: true,
     period: '2024.01 - 2024.06',
     image: '/images/mcountdown.png',
+    link: 'https://poc.mnetplus.world/mcountdown/ko/play',
   },
   {
     title: 'title 2',
     isActive: true,
     period: '2023.11 - 2024.05',
     image: '/images/item.jpg',
+    link: 'https://poc.mnetplus.world/mcountdown/ko/play',
   },
   {
     title: 'title 3',
     isActive: false,
     period: '2023.05 - 2023.10',
     image: '/images/mcountdown.png',
+    link: 'https://poc.mnetplus.world/mcountdown/ko/play',
   },
   {
     title: 'title 4',
     isActive: false,
     period: '2022.12 - 2023.04',
     image: '/images/item.jpg',
+    link: 'https://poc.mnetplus.world/mcountdown/ko/play',
   },
   {
     title: 'title 5',
     isActive: true,
     period: '2024.02 - 2024.07',
     image: '/images/mcountdown.png',
+    link: 'https://poc.mnetplus.world/mcountdown/ko/play',
   },
 ];
